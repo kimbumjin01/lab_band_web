0eaf696dd761d666872e8e742a08d46b4bdb3cb8
kbj110 <dodongdog01@gmail.com>
2026-05-29 01:23:18 +0900

    Add test app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="밴드 합주 투표 테스트", layout="centered")
+
+st.balloons()
+
+st.markdown(
+    "<h1 style='text-align: center;'>밴드 합주 투표 테스트 페이지입니다</h1>",
+    unsafe_allow_html=True,
+)
